d819dbc5ae893511b1b3571cb1de4968e63bb18c
Ferdinand Malcher <ferdinand@malcher.media>
2019-11-13 11:19:47 +0100

    BookRatingService + Tests

---
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookRatingService {
+
+  constructor() { }
+
+  rateUp(book: Book): Book {
+    return {
+      ...book,
+      rating: book.rating < 5 ? book.rating + 1 : book.rating
+    };
+  }
+
+  rateDown(book: Book): Book {
+    return {
+      ...book,
+      rating: Math.max(1, book.rating - 1)
+    }
+  }
+}
