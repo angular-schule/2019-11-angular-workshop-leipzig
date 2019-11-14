7369b3f781013b4ccfd82c089d37a69836ef58ae
Ferdinand Malcher <ferdinand@malcher.media>
2019-11-14 17:16:07 +0100

    Formular vollständig

--- book-rating/src/app/books/book-form/book-form.component.ts
+++ book-rating/src/app/books/book-form/book-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Book } from '../shared/book';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'br-book-form',
@@ -8,11 +9,41 @@ import { Book } from '../shared/book';
 })
 export class BookFormComponent implements OnInit {
 
+  bookForm: FormGroup;
+
   @Output() submitBook = new EventEmitter<Book>();
 
   constructor() { }
 
   ngOnInit() {
+    this.bookForm = new FormGroup({
+      isbn: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+      ]),
+      title: new FormControl('', Validators.required),
+      description: new FormControl(''),
+      price: new FormControl(null, Validators.min(2))
+    });
   }
 
+  isInvalid(name: string) {
+    const control = this.bookForm.get(name);
+    return control.invalid && control.touched;
+  }
+
+  hasError(name: string, errorCode: string) {
+    const control = this.bookForm.get(name);
+    return control.hasError(errorCode) && control.touched;
+  }
+
+  submitForm() {
+    const newBook: Book = {
+      ...this.bookForm.value,
+      rating: 1
+    };
+
+    this.submitBook.emit(newBook);
+  }
 }
