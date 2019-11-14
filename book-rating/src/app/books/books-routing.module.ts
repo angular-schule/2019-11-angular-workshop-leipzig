e7aee7c8aabad00515b5ea4ea2cff07f0f483cb5
Ferdinand Malcher <ferdinand@malcher.media>
2019-11-14 11:36:58 +0100

    Vorbereitung Suchseite

--- book-rating/src/app/books/books-routing.module.ts
+++ book-rating/src/app/books/books-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { SearchComponent } from './search/search.component';
 
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'search', component: SearchComponent },
   { path: ':isbn', component: BookDetailsComponent }
 ];
 
--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -6,13 +6,15 @@ import { BookComponent } from './book/book.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookRatingService } from './shared/book-rating.service';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     BookComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
