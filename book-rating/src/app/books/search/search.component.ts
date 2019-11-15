e4c98f1b996d6c7cd18d15f68f8c6950e319f680
Ferdinand Malcher <ferdinand@malcher.media>
2019-11-15 16:29:46 +0100

    Search

--- book-rating/src/app/books/search/search.component.ts
+++ book-rating/src/app/books/search/search.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { BookStoreService } from '../shared/book-store.service';
+import { filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-search',
@@ -10,13 +13,30 @@ import { BookStoreService } from '../shared/book-store.service';
 export class SearchComponent implements OnInit {
 
   searchControl: FormControl;
+  results$: Observable<Book[]>;
 
   constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
     this.searchControl = new FormControl('');
-    this.searchControl.valueChanges
-      .subscribe(e => console.log(e));
+
+    this.results$ = this.searchControl.valueChanges.pipe(
+      filter(term => term.length >= 3),
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap(term => this.bs.search(term))
+    );
   }
 
 }
+/*
+- Suchbegriff mindestens 3 Zeichen lang
+- erst suchen, wenn für bestimtme Zeit Finger stillgehalten
+- niemals zwei gleiche Suchen hintereinander
+- für jeden Suchbegriff HTTP-Request machen (this.bs.search => Book[])
+- Ergebnisse anzeigen (nur die Titel)
+
+Extra:
+- Ladeanzeige
+- Link zum Buch
+*/
